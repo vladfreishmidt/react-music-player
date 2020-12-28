c77170fcda9b708376ef493e9218b206eddf6761
Vlad <vladfreishmidt.webdevelopment@gmail.com>
2020-12-28 22:01:54 +0200

    Initial commit

---
+++ src/components/Player.js
@@ -0,0 +1,43 @@
+import React, { useRef } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faPlay,
+  faAngleLeft,
+  faAngleRight,
+} from "@fortawesome/free-solid-svg-icons";
+const Player = ({ currentSong }) => {
+  // Ref
+  const audioRef = useRef(null);
+
+  // Event handlers
+  const playSongHandler = () => {
+    audioRef.current.play();
+  };
+
+  return (
+    <div className="player">
+      <div className="time-control">
+        <p>start time</p>
+        <input type="range" />
+        <p>end time</p>
+      </div>
+      <div className="play-control">
+        <FontAwesomeIcon className="skip-back" size="2x" icon={faAngleLeft} />
+        <FontAwesomeIcon
+          onClick={playSongHandler}
+          className="play"
+          size="2x"
+          icon={faPlay}
+        />
+        <FontAwesomeIcon
+          className="skip-forward"
+          size="2x"
+          icon={faAngleRight}
+        />
+      </div>
+      <audio ref={audioRef} src={currentSong.audio}></audio>
+    </div>
+  );
+};
+
+export default Player;
